3973bc6209f2c6d7230550024703196f926989a3
Manvi Deshwal <manvideshwal@IM-BAN-LAP-0491.local>
2023-10-14 13:59:15 +0530

    Minor chagnes

--- api/index.js
+++ api/index.js
@@ -36,8 +36,16 @@
 
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
 const app = express();
+
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 const PORT = 3000;
 app.use(express.json());
 
